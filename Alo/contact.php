b692d25e4da677874655e94f933f287f4b2da45f
ildad <32815391+ildad@users.noreply.github.com>
2018-09-25 14:16:38 +0300

    update contact.php

--- Alo/contact.php
+++ Alo/contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <brainmediarecors@gmail.com>';
+$from = 'Demo contact form <>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'Demo contact form <brainmediarecors@gmail.com>';
